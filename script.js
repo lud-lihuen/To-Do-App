b4370a2dd28c1e80910176e283f9ae1f8e227792
Lihuén Villarreal <110670239+lud-lihuen@users.noreply.github.com>
2023-12-08 19:39:25 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,52 +3,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const addTaskBtn = document.getElementById('addTaskBtn');
     const taskList = document.getElementById('taskList');
 
-    // Cargar las tareas almacenadas en el Local Storage al cargar la página
-    loadTasksFromLocalStorage();
-
-    // Evento al hacer clic en el botón "Agregar"
-    addTaskBtn.addEventListener('click', function () {
-        const taskText = taskInput.value.trim();
-        if (taskText !== '') {
-            addTask(taskText);
-            saveTaskToLocalStorage(taskText);
-            // Limpiar el campo de entrada de tareas
-            taskInput.value = '';
-        }
-    });
-
-    // Evento al presionar la tecla "Enter" en el campo de entrada de tareas
-    taskInput.addEventListener('keyup', function (event) {
-        if (event.keyCode === 13) {
-            // Simular clic en el botón "Agregar" al presionar "Enter"
-            addTaskBtn.click();
-        }
-    });
-
-    // Evento de cambio en el contenedor de la lista de tareas
-    taskList.addEventListener('change', function (event) {
-        const checkbox = event.target;
-        if (checkbox.type === 'checkbox') {
-            // Obtener el elemento padre (li) de la tarea y cambiar su estado completado
-            const taskItem = checkbox.parentNode;
-            taskItem.classList.toggle('completed');
-            // Actualizar el estado de la tarea en el Local Storage
-            updateTaskInLocalStorage(taskItem);
-        }
-    });
-
-    // Evento al hacer clic en el botón "Eliminar" de una tarea
-    taskList.addEventListener('click', function (event) {
-        const deleteBtn = event.target;
-        if (deleteBtn.classList.contains('btn-delete')) {
-            // Obtener el elemento padre (li) de la tarea y eliminarla
-            const taskItem = deleteBtn.closest('li');
-            deleteTaskFromLocalStorage(taskItem);
-            taskItem.remove();
-        }
-    });
-
-    // Función para agregar una tarea a la lista
+    // Función para añadir una tarea a la lista
     function addTask(taskText) {
         const taskItem = document.createElement('li');
         taskItem.classList.add('list-group-item');
@@ -70,6 +25,13 @@ document.addEventListener('DOMContentLoaded', function () {
         taskList.appendChild(taskItem);
     }
 
+    // Función para guardar una tarea en el Local Storage
+    function saveTaskToLocalStorage(taskText) {
+        const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+        tasks.push({ text: taskText, completed: false });
+        localStorage.setItem('tasks', JSON.stringify(tasks));
+    }
+
     // Función para cargar las tareas desde el Local Storage al cargar la página
     function loadTasksFromLocalStorage() {
         const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
@@ -83,21 +45,6 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     }
 
-    // Función para guardar una tarea en el Local Storage
-    function saveTaskToLocalStorage(taskText) {
-        const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
-        tasks.push({ text: taskText, completed: false });
-        localStorage.setItem('tasks', JSON.stringify(tasks));
-    }
-
-    // Función para actualizar el estado de una tarea en el Local Storage
-    function updateTaskInLocalStorage(taskItem) {
-        const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
-        const taskIndex = Array.from(taskList.children).indexOf(taskItem);
-        tasks[taskIndex].completed = taskItem.classList.contains('completed');
-        localStorage.setItem('tasks', JSON.stringify(tasks));
-    }
-
     // Función para eliminar una tarea del Local Storage
     function deleteTaskFromLocalStorage(taskItem) {
         const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
@@ -105,4 +52,57 @@ document.addEventListener('DOMContentLoaded', function () {
         tasks.splice(taskIndex, 1);
         localStorage.setItem('tasks', JSON.stringify(tasks));
     }
-});  
+
+    // Función para actualizar el estado (checked/unchecked) de una tarea en el Local Storage
+    function updateTaskInLocalStorage(taskItem) {
+        const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+        const taskIndex = Array.from(taskList.children).indexOf(taskItem);
+        tasks[taskIndex].completed = taskItem.classList.contains('completed');
+        localStorage.setItem('tasks', JSON.stringify(tasks));
+    }
+
+    // Cargar las tareas almacenadas en el Local Storage al cargar la página
+    loadTasksFromLocalStorage();
+
+    // Evento al hacer clic en el botón "Añadir"
+    addTaskBtn.addEventListener('click', function () {
+        const taskText = taskInput.value.trim();
+        if (taskText !== '') {
+            addTask(taskText);
+            saveTaskToLocalStorage(taskText);
+            // Limpiar el campo de entrada de tareas
+            taskInput.value = '';
+        }
+    });
+
+    // Evento al presionar la tecla "Enter" en el campo de entrada de tareas
+    taskInput.addEventListener('keyup', function (event) {
+        if (event.keyCode === 13) {
+            // Simular clic en el botón "Añadir" al presionar "Enter"
+            addTaskBtn.click();
+        }
+    });
+
+    // Evento de check/uncheck de una tarea
+    taskList.addEventListener('change', function (event) {
+        const checkbox = event.target;
+        if (checkbox.type === 'checkbox') {
+            // Obtener el elemento padre (li) de la tarea y cambiar su estado completado
+            const taskItem = checkbox.parentNode;
+            taskItem.classList.toggle('completed');
+            // Actualizar el estado de la tarea en el Local Storage
+            updateTaskInLocalStorage(taskItem);
+        }
+    });
+
+    // Evento al hacer clic en el botón "Borrar" de una tarea
+    taskList.addEventListener('click', function (event) {
+        const deleteBtn = event.target;
+        if (deleteBtn.classList.contains('btn-delete')) {
+            // Obtener el elemento padre (li) de la tarea y eliminarla
+            const taskItem = deleteBtn.closest('li');
+            deleteTaskFromLocalStorage(taskItem);
+            taskItem.remove();
+        }
+    });
+});
